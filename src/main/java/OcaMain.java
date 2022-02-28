15e3aad8587dcf9a37b426c5a327d3b8789ce3aa
kpeinado <kpeinado@everis.com>
2022-02-28 11:56:39 -0500

    faltante oca

--- src/main/java/OcaMain.java
+++ src/main/java/OcaMain.java
@@ -1,16 +1,15 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
 
 import java.time.Duration;
 
 public class OcaMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.ie.driver", "./src/main/resources/driver/IEDriverServer_1.exe");
 
@@ -35,21 +34,76 @@ public class OcaMain {
 
         System.out.println("Nos acabamos de logear");
 
+        driver.switchTo().frame(3);
 
-        // seleeccionamos el rol
-        ewait.until(ExpectedConditions.elementToBeClickable(By.id("Acceder")));
+        // seleccionamos el rol
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                                        .withTimeout(Duration.ofSeconds(10))
+                                        .pollingEvery(Duration.ofSeconds(3))
+                                        .ignoring(NoSuchElementException.class);
 
-        Select rol = new Select(driver.findElement(By.id("comboAcceso")));
+        ewait.until(
+                ExpectedConditions.presenceOfNestedElementsLocatedBy(
+                        By.id("comboAcceso"), By.tagName("option")
+                )
+        );
+
+        Select rol = new Select(driver.findElement(By.xpath("//select[@id='comboAcceso']")));
 
         rol.selectByVisibleText("MADRID B");
 
-        Select privilegio = new Select(driver.findElement(By.id("sPerfilSeleccionado")));
+        Select privilegio = new Select(driver.findElement(By.xpath("//select[@id='sPerfilSeleccionado']")));
 
         privilegio.selectByVisibleText("Administracion");
 
         driver.findElement(By.id("Acceder")).click();
 
         System.out.println("Entramos al sistema!!!!");
+
+
+        // hacemos el flujo para consultar envios
+
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame(3);
+
+        // espereamos hasta q el logo pueda verse
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("cabecera")));
+
+        // damos click en Envios
+        driver.findElement(By.xpath("//ul[@id='menu_principal']/li[2]/a")).click();
+
+        //esperamos hasta que nos aparezca el submenu
+        ewait.until(
+                ExpectedConditions.elementToBeClickable(
+                        By.xpath("//ul[@id='theMenu']/li[2]/h3/a")
+                )
+        );
+
+        // damos click en consulta envios
+        driver.findElement(By.xpath("//ul[@id='theMenu']/li[2]/h3/a")).click();
+
+        System.out.println("Ya entramos a consulta envios!!!");
+
+
+        // esperamos hasta que se pueda dar click el boton buscar
+        ewait.until(ExpectedConditions.elementToBeClickable(By.id("filtrar")));
+
+        WebElement codNacional = driver.findElement(By.id("_formulario__envio_sCodEnvioNacional"));
+        WebElement codInternacional = driver.findElement(By.id("_formulario__envio_sCodEnvioInternacional"));
+        WebElement codEnvioCanal = driver.findElement(By.id("_formulario__envio_sCodEnvioIntegrador"));
+
+        codNacional.clear();
+        codInternacional.clear();
+        codEnvioCanal.clear();
+
+        codNacional.sendKeys("QWEQEQWE");
+        codInternacional.sendKeys("QWEQW");
+        codEnvioCanal.sendKeys("WWWWWWWWW");
+
+
+        driver.findElement(By.id("filtrar")).click();
+
+        System.out.println("Estamos buscando!!!");
 
     }
 }
